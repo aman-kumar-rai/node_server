5c143fea87fb3735086defba9725d7d63d8f6d77
Aman Kumar <kumaraman140795@gmail.com>
2024-03-24 21:48:33 +0530

    Create basic web server

--- src/handlers/methods/get.mjs
+++ src/handlers/methods/get.mjs
@@ -1,5 +1,17 @@
+import { readFile } from "node:fs/promises";
+
+// res.setHeader("X-Content-Type-Options", "nosniff");
+
 const getHandler = (req, res) => {
-    res.end(`GET method was called for resource: ${req.url}`);
+    const filePath = `${process.cwd()}/public${req.url}`;
+    readFile(filePath, {
+        encoding: "utf-8"
+    }).then((fileContent) => {
+        res.end(fileContent);
+    }).catch((error) => {
+        console.error(error);
+        res.end(`Server error`);
+    })
 }
 
 export {
